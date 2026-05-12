25608e99ce5fa2940fd6b3b8402b57fdd40db9db
luizmrd <lipesilvamiranda@hotmail.com>
2026-05-12 19:50:06 -0300

    Order

--- src/main/java/com/luizmrd/entities/User.java
+++ src/main/java/com/luizmrd/entities/User.java
@@ -4,9 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -21,6 +24,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User(){}
 
@@ -72,6 +78,10 @@ public class User implements Serializable {
         this.password = password;
     }
 
+    public List<Order> getOrders() {
+        return orders;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (!(o instanceof User user)) return false;
@@ -82,4 +92,5 @@ public class User implements Serializable {
     public int hashCode() {
         return Objects.hashCode(getId());
     }
+
 }
