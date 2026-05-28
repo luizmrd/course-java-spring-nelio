c3bd01083c6822dab21bc1af3486cfa0e118d9e5
Luiz Miranda <lipesilvamiranda@hotmail.com>
2026-05-28 20:28:41 -0300

    total e subtoal

--- src/main/java/com/luizmrd/entities/OrderItem.java
+++ src/main/java/com/luizmrd/entities/OrderItem.java
@@ -65,6 +65,11 @@ public class OrderItem  implements Serializable {
         this.quantity = quantity;
     }
 
+
+    public Double getSubTotal(){
+        return price* quantity;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
@@ -76,4 +81,6 @@ public class OrderItem  implements Serializable {
     public int hashCode() {
         return Objects.hashCode(id);
     }
+
+
 }
