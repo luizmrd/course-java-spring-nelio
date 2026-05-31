48f97a8adf7c752f73ef269ec61da30ff2b49b41
Luiz Miranda <lipesilvamiranda@hotmail.com>
2026-05-31 19:21:50 -0300

    Exeption

--- src/main/java/com/luizmrd/resources/exeptions/ResouceExceptionHandler.java
+++ src/main/java/com/luizmrd/resources/exeptions/ResouceExceptionHandler.java
@@ -20,5 +20,12 @@ public class ResouceExceptionHandler {
         StandardError err = new StandardError(Instant.now(), status.value(), error,e.getMessage(),request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
+    @ExceptionHandler(DataBaseException.class)
+    public ResponseEntity<StandardError> database(DataBaseException e, HttpServletRequest request){
+        String error = "Data base error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError(Instant.now(), status.value(), error,e.getMessage(),request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
 
 }
--- src/main/java/com/luizmrd/services/UserService.java
+++ src/main/java/com/luizmrd/services/UserService.java
@@ -2,9 +2,11 @@ package com.luizmrd.services;
 
 import com.luizmrd.entities.User;
 import com.luizmrd.repositories.UserRepository;
+import com.luizmrd.resources.exeptions.DataBaseException;
 import com.luizmrd.services.exeptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,17 +31,22 @@ public class UserService {
     }
 
     public void delete(Long id){
+        if (!repository.existsById(id)) {
+            throw new ResourceNotFoundException(id);
+        }
+
         repository.deleteById(id);
     }
 
+
     public User update(Long id, User obj){
         User entity = repository.getReferenceById(id);
-        updadeData(entity,obj);
+        updateData(entity,obj);
         return repository.save(entity);
 
     }
 
-    private void updadeData(User entity, User obj) {
+    private void updateData(User entity, User obj) {
         entity.setEmail(obj.getEmail());
         entity.setName(obj.getName());
         entity.setPhone(obj.getPhone());
